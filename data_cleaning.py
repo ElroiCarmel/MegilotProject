0e98b5feee89ab9ac887e2ddf582f804d84327b6
elroicarmel <elroica277@gmail.com>
2024-12-23 23:30:46 +0200

    more efficient and clean code

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,51 +3,35 @@ import numpy as np
 import re
 
 def clean_data(df: pd.DataFrame, mapper: dict[str, str], to_split: str, columns: list[str]) -> pd.DataFrame:
-    
     # Copy the missing from the translation column
-    mask = df[to_split].isna()
-    indices = df[mask].index
-    df.loc[indices, to_split] = df.loc[indices, 'translation']
-    df.loc[:, columns] = np.nan
-    df = df.astype('object')
-
-    # Normalize the fts column
-    df.loc[:, to_split] = df.loc[:, to_split].str.strip("[]")
+    exclude = ['corrected inflection','and','lemma uncertain', 'spelling error/correction']
+    mask = df[to_split].isna() & ~df['translation'].isin(exclude)
+    df[to_split] = df[to_split].fillna(df.loc[mask, 'translation'])
+    d = ['verse', 'pos', 'number', 'gender', 'tense', 'person', 'binyan', 'other']
+    df = df.drop(d, axis=1)
     
     # Splitting
-    for i in df.index.values:
-        # print(i)
-        fts = df.at[i, to_split]
-        for val in re.split('[, ]+', fts):
-            if val.lower() in mapper:
-                c = mapper[val.lower()]
-                exists = df.at[i, c]
-                if pd.isna(exists):
-                    df.at[i, c] = val
-                else:
-                    df.at[i, c] = ','.join([exists, val])
-            else:
-                if fts != df.at[i, 'translation']:
-                    print('Error!')
-                    print(f'row: {i}\nfts: {fts}\nvalue: {val} has no matching column!')
-    
+    for c in cols:
+        pat_txt = '|'.join(mapper[c])
+        patt = re.compile(pat_txt, re.IGNORECASE)
+        df[c] = (
+            df[to_split].map(patt.findall, na_action='ignore')
+            .map(lambda x: ','.join(x) if len(x) else np.nan, na_action='ignore')
+            )
     return df
 
 
 def create_mapper(df: pd.DataFrame, cols: list[str]) -> dict[str, str]:
-    mapper = {}
-    for col in cols:
-        for un in df[col].unique():
-            if pd.isna(un) == False:
-                for val in str.split(un, ','):
-                    mapper[val.lower()] = col
-    mapper['nitpalpel'] = 'binyan'
-    mapper['nitpoel'] = 'binyan'
-    mapper['hitpoel'] = 'binyan'
-    mapper['hithaphel'] = 'binyan'
-    mapper['passiveimperfect'] = 'tense'
-    mapper['copulative'] = 'subtype'
-    mapper['cohortativeheh'] = 'mood'
+    mapper = {a:[
+        val for sub in df[c].unique().tolist() if pd.notna(sub)
+        for val in re.split(r'[, ]+', sub)
+    ] for a, c in zip(cols, cols)}
+
+    mapper['binyan'].extend(['nitpalpel', 'nitpoel', 'hithaphel'])
+    mapper['tense'].append('passiveimperfect')
+    mapper['subtype'].append('copulative')
+    mapper['mood'].append('cohortativeheh')
+
     return mapper
 
 
@@ -68,6 +52,6 @@ megilot_fixed = clean_data(megilot, mapper, 'fts', cols)
 megilot_fixed.info()
 
 # Saving the fixed to csv
-mishna_fixed.to_csv('data/mishna_fixed.csv', index=False, quotechar='"', quoting=1)
-megilot_fixed.to_csv('data/megilot_fixed.csv', index=False, quotechar='"', quoting=1)
+mishna_fixed.to_csv('data/mishna_fixed_v2.csv', index=False, quotechar='"', quoting=1)
+megilot_fixed.to_csv('data/megilot_fixed_v2.csv', index=False, quotechar='"', quoting=1)
 
